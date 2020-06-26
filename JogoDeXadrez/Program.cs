8f393657346703facfcdb15c631a5c7c296e6978
Renanvt <jonatan_rvs@hotmail.com>
2020-06-26 03:26:42 -0300

    Comecando a implementar um metodo que imprime o tabuleiro

--- JogoDeXadrez/Program.cs
+++ JogoDeXadrez/Program.cs
@@ -5,6 +5,7 @@ namespace JogoDeXadrez {
         static void Main(string[] args) {
             Tabuleiro tab = new Tabuleiro(8, 8);
             Console.ReadLine();
+            Tela.imprimirTabuleiro(tab);
         }
     }
 }
