ba828bbf5f8bbc297bc187cfd6b1c5e0e44b0011
Renanvt <jonatan_rvs@hotmail.com>
2020-06-27 17:45:14 -0300

    Implementando as retricoes de movimento

--- JogoDeXadrez/xadrez/PartidaDeXadrez.cs
+++ JogoDeXadrez/xadrez/PartidaDeXadrez.cs
@@ -3,9 +3,10 @@ using tabuleiro;
 namespace xadrez {
     class PartidaDeXadrez {
         public Tabuleiro tab { get; private set; }
-        private int turno; //a cada jogada o turno é incrementado
-        private Cor jogadorAtual; //Indica a vez de quem jogar
+        public int turno { get; private set; } //a cada jogada o turno é incrementado
+        public Cor jogadorAtual { get; private set; } //Indica a vez de quem jogar
         public bool terminada { get; private set; } //Indica se a partida está terminada ou não
+        
         public PartidaDeXadrez() {
             tab = new Tabuleiro(8, 8);
             turno = 1;
@@ -19,6 +20,35 @@ namespace xadrez {
             p.incrementarQteMovimentos();
             Peca pecaCapturada = tab.retirarPeca(destino);//Se tiver uma peca no destino, tira ela
             tab.colocarPeca(p, destino);
+        }
+        public void realizaJogada(Posicao origem, Posicao destino) {
+            executaMovimento(origem, destino);
+            turno++;
+            mudaJogador();
+        }
+        public void validarPosicaoDeOrigem(Posicao pos) {
+            if(tab.peca(pos) == null) {
+                throw new TabuleiroException("Não existe peça na posição de origem escolhida!");
+            }
+            if(jogadorAtual != tab.peca(pos).cor) {
+                throw new TabuleiroException("A peça de origem escolhida não é sua!");
+            }
+            if (!tab.peca(pos).existeMovimentosPossiveis()) {
+                throw new TabuleiroException("Não há movimentos possíveis para a peça escolhida!");
+            }
+        }
+        public void validarPosicaoDeDestino(Posicao origem, Posicao destino) {
+            if (!tab.peca(origem).podeMoverPara(destino)) {
+                throw new TabuleiroException("Posicao de destino inválida");
+            }
+        }
+        private void mudaJogador() {
+            if(jogadorAtual == Cor.Branca) {
+                jogadorAtual = Cor.Preta;
+            }
+            else {
+                jogadorAtual = Cor.Branca;
+            }
         }
         private void colocarPecas() {
             tab.colocarPeca(new Torre(tab, Cor.Branca), new PosicaoXadres('c',1).toPosicao());
